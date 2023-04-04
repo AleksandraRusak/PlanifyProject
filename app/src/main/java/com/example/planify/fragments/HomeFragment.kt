42a7928da400f52849695f935b2d61072fc9a901
AleksandraRusak <rusakaleksandra94@gmail.com>
2023-04-04 12:53:34 +0200

    test commit

--- app/src/main/java/com/example/planify/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/planify/fragments/HomeFragment.kt
@@ -10,13 +10,6 @@ import com.example.planify.R
 class HomeFragment : Fragment() {
 
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
 
 
 }
